aa2ca518e27cda55d747ad8c7ef7401e82ee45bd
lby132 <quddbscjswow@naver.com>
2023-05-11 00:38:53 +0900

    spring

--- src/main/java/hello/login/domain/login/LoginService.java
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class LoginService {
+public class  LoginService {
 
     private final MemberRepository memberRepository;
 
